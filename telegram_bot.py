f05ecc22f65d237375251472c359bb59811e84de
locenose-sys <locenose-sys@example.com>
2025-10-18 00:03:11 +0530

    Add Mega integration and web interface

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,13 +2,20 @@ import requests
 import time
 import os
 import threading
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+from mega import Mega
 
 app = Flask(__name__)
 
 # Bot token and chat ID
 TOKEN = os.getenv('TOKEN', '8361902202:AAH8KJW9_6ixm140bRmwY1Jz52kwHns-GqM')
 CHAT_ID = os.getenv('CHAT_ID', '@noselovea')  # Public group username
+
+# Mega credentials
+MEGA_EMAIL = os.getenv('MEGA_EMAIL')
+MEGA_PASSWORD = os.getenv('MEGA_PASSWORD')
+mega = Mega()
+m = mega.login(MEGA_EMAIL, MEGA_PASSWORD) if MEGA_EMAIL and MEGA_PASSWORD else None
 
 # List of files to send
 files = [
@@ -19,6 +26,13 @@ files = [
 ]
 
 def send_file(file_path):
+    if file_path.startswith('http'):  # If it's a URL from Mega
+        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+        data = {'chat_id': CHAT_ID, 'text': file_path}
+        response = requests.post(url, data=data)
+        print(response.json())
+        return
+
     if not os.path.exists(file_path):
         print(f"File {file_path} not found.")
         return
@@ -28,7 +42,7 @@ def send_file(file_path):
         data = {'chat_id': CHAT_ID}
         files_data = {}
 
-        if ext in ['.jpg', '.jpeg', '.png', '.gif']:
+        if ext in ['.jpg', '.jpeg', '.png']:
             url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
             files_data = {'photo': f}
         elif ext in ['.mp4', '.avi', '.mov']:
@@ -58,7 +72,30 @@ def bot_loop():
 
 @app.route('/')
 def home():
-    return "Bot is running!"
+    return render_template('index.html')
+
+@app.route('/files')
+def get_files():
+    return jsonify(files)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    uploaded_files = request.files.getlist('files')
+    for file in uploaded_files:
+        if file:
+            filename = file.filename
+            file_path = os.path.join('.', filename)
+            file.save(file_path)
+            if m:
+                # Upload to Mega
+                folder = m.find('telegram_files') or m.create_folder('telegram_files')
+                m.upload(file_path, folder[0])
+                link = m.get_link(m.find(filename, folder[0]))
+                files.append(link)
+                os.remove(file_path)  # Delete local file
+            else:
+                files.append(filename)
+    return '', 204
 
 if __name__ == '__main__':
     # Start bot in a thread
